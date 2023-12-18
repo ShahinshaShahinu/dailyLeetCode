d44cebcb19febfede6c4fa430ce6080ce16a9547
Muhammed Shahinsha .c <122088649+ShahinshaShahinu@users.noreply.github.com>
2023-12-18 08:18:29 +0530

    Time: 48 ms (71.38%) | Memory: 42.2 MB (8.34%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.js
+++ 2556-convert-the-temperature/convert-the-temperature.js
@@ -4,6 +4,7 @@
  */
 var convertTemperature = function(celsius) {
     const ans = [];
-    ans.push(celsius+273.15,32.00+celsius*1.80);
-    return ans 
+    const kelvin = celsius + 273.15;
+    const fahrenheit = (celsius * 1.8) + 32.00;
+    ans.push(kelvin, fahrenheit);    return ans 
 };

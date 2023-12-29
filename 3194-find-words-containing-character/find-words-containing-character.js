bc1298af425b1a144dc6b27f61232554c2604370
Muhammed Shahinsha .c <122088649+ShahinshaShahinu@users.noreply.github.com>
2023-12-29 22:23:20 +0530

    Time: 127 ms (5.06%) | Memory: 44.5 MB (76.99%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -4,20 +4,17 @@
  * @return {number[]}
  */
 var findWordsContaining = function(words, x) {
-    // let ar=[]
-    // for(let i=0;i<words.length;i++){
-    //     let str=words[i]
-    //     for(let j=0;j<str.length;j++){
-    //         if(str[j]==x){
-    //             ar.push(i);
-    //             break;
-    //         }
-    //     }
-    // }
+    let ar=[]
+    for(let i=0;i<words.length;i++){
+        let str=words[i]
+        for(let j=0;j<str.length;j++){
+            if(str[j]==x){
+                ar.push(i);
+                break;
+            }
+        }
+    }
 
-    // return ar
-
-
-        return words.reduce((acc, curr, i) => curr.indexOf(x) > -1 ? [...acc, i]: acc, [])
+    return ar
 
 };

7a0937362b0968b5dc0c570d9be4577bac91d356
Muhammed Shahinsha .c <122088649+ShahinshaShahinu@users.noreply.github.com>
2023-12-29 22:22:20 +0530

    Time: 126 ms (5.06%) | Memory: 46.6 MB (7.44%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -4,16 +4,20 @@
  * @return {number[]}
  */
 var findWordsContaining = function(words, x) {
-    let ar=[]
-    for(let i=0;i<words.length;i++){
-        let str=words[i]
-        for(let j=0;j<str.length;j++){
-            if(str[j]==x){
-                ar.push(i);
-                break;
-            }
-        }
-    }
+    // let ar=[]
+    // for(let i=0;i<words.length;i++){
+    //     let str=words[i]
+    //     for(let j=0;j<str.length;j++){
+    //         if(str[j]==x){
+    //             ar.push(i);
+    //             break;
+    //         }
+    //     }
+    // }
 
-    return ar
+    // return ar
+
+
+        return words.reduce((acc, curr, i) => curr.indexOf(x) > -1 ? [...acc, i]: acc, [])
+
 };

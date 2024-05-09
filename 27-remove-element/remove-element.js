a702d49c277e3f57a4da55c8ae19f7285c1e2efa
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2024-05-09 10:42:24 +0530

    Time: 49 ms (77.99%) | Memory: 48.5 MB (85.23%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,12 +4,20 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-   let j=0;
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]!=val){
-            nums[j]=nums[i];
-            j++;
+let i = 0;
+
+while (i < nums.length) {
+    if(nums[i] == val){
+        if(nums.length-1 == i){
+            nums.length--
+        }else{           
+            for (let j = i; j < nums.length; j++) {
+                    nums[j] = nums[j+1]
+            }
+            i--
+            nums.length--
         }
     }
-    return j;
+    i++
+}
 };

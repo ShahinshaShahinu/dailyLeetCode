add713592641b35ec54ad853c8345cc0e0ac7a3a
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2024-05-09 10:01:39 +0530

    Time: 101 ms (5.28%) | Memory: 56.4 MB (5.09%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,24 +6,26 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-   let i = m - 1;
-    let j = n - 1;
-    let k = m + n - 1;
-
-    while (i >= 0 && j >= 0) {
-        if (nums1[i] > nums2[j]) {
-            nums1[k] = nums1[i];
-            i--;
-        } else {
-            nums1[k] = nums2[j];
-            j--;
+    let i = m-1 ;
+    let j = n-1 ;
+    let k = m + n - 1 ;
+    
+    
+    while( 0 <= i && 0 <= j){
+        if(nums1[i] < nums2[j] ){
+            console.log(nums1)
+            nums1[k]=nums2[j];
+            j--
+        }else{
+            nums1[k]=nums1[i];
+            i-- ;
         }
-        k--;
-    }
+        k-- ;
+    }   
 
-    // If there are remaining elements in nums2
-    while (j >= 0) {
-        nums1[k] = nums2[j];
+
+    while(j>=0){
+        nums1[k] = nums2[j]
         j--;
         k--;
     }

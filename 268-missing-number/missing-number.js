62742488498b2ef9c076c8654925e4f9b2731bc1
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2024-05-15 21:59:40 +0530

    Time: 63 ms (48.85%) | Memory: 51.3 MB (51.70%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -3,11 +3,10 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-     let missing = nums.length;
-
-    for (let i = 0; i < nums.length; i++) {
-        missing ^= i ^ nums[i];
-    }
-
-    return missing;
+     const n = nums.length;
+  let sum = (n * (n + 1)) / 2; // Sum of natural numbers from 0 to n
+  for (let i = 0; i < n; i++) {
+    sum -= nums[i]; // Subtract each number in nums
+  }
+  return sum;
 };

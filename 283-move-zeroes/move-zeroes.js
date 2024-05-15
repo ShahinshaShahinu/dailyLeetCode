ff8ec125921bc2a92cd2930b4fae91276656d45a
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2024-05-15 22:49:41 +0530

    Time: 72 ms (74.22%) | Memory: 52.8 MB (97.21%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,19 +3,20 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
+
       let count = 0;
+
       for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== 0) {
           nums[count] = nums[i];
           count++;
         }
       }
-      
+
       while (count < nums.length) {
         nums[count]=0;
         count++;
       }
 
       return nums;
-
 };

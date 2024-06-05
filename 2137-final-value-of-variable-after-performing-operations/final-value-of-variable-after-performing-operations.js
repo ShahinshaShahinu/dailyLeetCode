a1c9fe6c155bee5f8ddbc255abea0a75e5109a4d
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2024-06-05 23:37:35 +0530

    Time: 61 ms (23.47%) | Memory: 48.9 MB (83.37%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.js
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var finalValueAfterOperations = function(operations) {
-    let  x = 0 ;
+    var  x = 0 ;
     for(let i=0; i<operations.length; i++){
         if(operations[i] == 'X--' || operations[i] == '--X' ){
             x-- ;

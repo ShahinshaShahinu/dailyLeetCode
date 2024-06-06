34d23dbcdc4252c8ff36ac1ebd0835728913a0d4
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2024-06-06 23:08:03 +0530

    Time: 52 ms (95.69%) | Memory: 52.8 MB (13.23%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,14 +3,14 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    for (let i = 0; i < nums.length; i++) {
-  if (nums[i] == nums[i + 1]) {
-    // Remove Elements
-    for (let j = i; j < nums.length; j++) {
-      nums[j] = nums[j + 1];
-    }
-    nums.length--;
-    i--;
-  }
-}
+      let k=1;
+
+     for(i=0;i<nums.length-1;i++){
+         if(nums[i]!==nums[i+1]){
+             nums[k]=nums[i+1];
+             k++;
+         }
+     }
+     
+   return k;
 };

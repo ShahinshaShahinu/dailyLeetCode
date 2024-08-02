0c5c0011ae17b2eb465292a5bc7e2142ad8197b3
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2024-08-02 11:51:48 +0530

    Time: 131 ms (62.09%) | Memory: 58.9 MB (15.41%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,18 +3,5 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if (x < 0) {
-        return false;
-    }
-
-    let reverse = 0;
-    let xcopy = x;
-
-    while (x > 0) {
-        reverse = (reverse * 10) + (x % 10);
-        // x = Math.floor(x / 10);
-        x = x/10 ;
-        x = x - (x % 1)
-    }
-    return reverse === xcopy;
+    return x === +x.toString().split("").reverse().join("");
 };

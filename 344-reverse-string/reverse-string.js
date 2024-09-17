1daddac1daf0a57d2a8d1ee6480382e13100dce8
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2024-09-17 12:22:29 +0530

    Time: 71 ms (83.90%) | Memory: 57.6 MB (18.27%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,6 +3,14 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function (s) {
-     return s.reverse()
-};
+    let start = 0;
+    let end = s.length-1;
+    while(start < end) {
+        let temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
+    }
+    return s;};
 

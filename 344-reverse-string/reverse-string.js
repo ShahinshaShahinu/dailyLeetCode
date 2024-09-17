0bf5877f29e8e0bd4cd95ebdcaf575693e485a4a
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2024-09-17 12:22:06 +0530

    Time: 77 ms (59.89%) | Memory: 57.1 MB (55.19%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -2,28 +2,7 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-// var reverseString = function (s) {
-//     let L = 0;
-//     let R = s.length - 1;
-//     while (L < R) {
-//         let Temp = s[L];
-//         s[L] = s[R];
-//         s[R] = Temp;
-//         L++; R--
-//     }
-
-//     return s
-// };
-
 var reverseString = function (s) {
-    //   let start = 0;
-    // let end = s.length-1;
-    // while(start < end) {
-    //     let temp = s[start];
-    //     s[start] = s[end];
-    //     s[end] = temp;
-    //     start++;
-    //     end--;
-    // }
-   return s.reverse()
+     return s.reverse()
 };
+

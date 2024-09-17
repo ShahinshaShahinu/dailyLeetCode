1f835927aee2df4e5983d4c4949748c92612bb8a
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2024-09-17 12:21:19 +0530

    Time: 82 ms (36.35%) | Memory: 56.9 MB (70.64%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -16,13 +16,14 @@
 // };
 
 var reverseString = function (s) {
-      let start = 0;
-    let end = s.length-1;
-    while(start < end) {
-        let temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
-        start++;
-        end--;
-    }
+    //   let start = 0;
+    // let end = s.length-1;
+    // while(start < end) {
+    //     let temp = s[start];
+    //     s[start] = s[end];
+    //     s[end] = temp;
+    //     start++;
+    //     end--;
+    // }
+    s.reverse()
 };

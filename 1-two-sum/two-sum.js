76bb589a591eb33a28115888391c07b38bc0dd19
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2024-09-27 12:01:55 +0530

    Time: 48 ms (96.05%) | Memory: 51 MB (39.61%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,13 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-
-    let map = {}
-    for(let i = 0 ; i < nums.length ; i++){
-        let diff = target-nums[i]
-        if(diff in map){
-            return [map[diff] , i]
+    let prev = {};
+    
+    for(let i=0;i<nums.length;i++){
+        let diff = target - nums[i];
+        if(diff in prev){
+            return [prev[diff],i];
         }
-        map[nums[i]] = i 
+        prev[nums[i]] = i ; 
     }
+    return 0;
 };

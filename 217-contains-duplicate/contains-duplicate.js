bb993b92ffb7707b01dc399dbaa4f7fb18fc67ff
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-02-06 18:58:42 +0530

    Time: 9 ms (88.98%) | Memory: 59.7 MB (92.37%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,19 +3,29 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-   // Create a set to store unique elements
-    let numSet = new Set();
-    
-    // Iterate through the array
+    let UniqueSet = new Set();
     for (let i = 0; i < nums.length; i++) {
-        // If the set already contains the element, return true
-        if (numSet.has(nums[i])) {
+        if (UniqueSet.has(nums[i])) {
             return true;
+        } else {
+            UniqueSet.add(nums[i]);
         }
-        // Add the element to the set
-        numSet.add(nums[i]);
     }
-    
-    // If no duplicates are found, return false
     return false;
+
+    // // Create a set to store unique elements
+    // let numSet = new Set();
+    
+    // // Iterate through the array
+    // for (let i = 0; i < nums.length; i++) {
+    //     // If the set already contains the element, return true
+    //     if (numSet.has(nums[i])) {
+    //         return true;
+    //     }
+    //     // Add the element to the set
+    //     numSet.add(nums[i]);
+    // }
+    
+    // // If no duplicates are found, return false
+    // return false;
 };

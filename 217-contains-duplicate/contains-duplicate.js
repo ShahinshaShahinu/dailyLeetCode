8411ae574a96d607b815150d06cb1732c4c846b0
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-02-06 18:57:03 +0530

    Time: 12 ms (70.57%) | Memory: 60.2 MB (85.24%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -5,7 +5,9 @@
 var containsDuplicate = function (nums) {
     const UniqueSet = new Set();
     for (let i = 0; i < nums.length; i++) {
-        if (UniqueSet.has(nums[i])) return true; else UniqueSet.add(nums[i])
+        if (UniqueSet.has(nums[i])) {
+            return true;
+        } else UniqueSet.add(nums[i])
     }
     return false
 };

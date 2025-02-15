a63bdc83961a38273c9b4350ef7fa687d07fe3ad
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-02-15 14:25:55 +0530

    Time: 2 ms (32.29%) | Memory: 51.7 MB (32.29%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.js
+++ 3379-score-of-a-string/score-of-a-string.js
@@ -4,15 +4,8 @@
  */
 var scoreOfString = function (s) {
     let Score = 0;
-    for (let index = 0; index < s.length; index++) {
-        if (s.charCodeAt(index) && s.charCodeAt(index + 1)) {
-            const Value = s.charCodeAt(index) - s.charCodeAt(index + 1);
-            if (Value < 0) {
-                Score += Value * -1;
-            } else if (Value >= 0) {
-                Score += Value;
-            }
-        }
+    for (let index = 1; index < s.length; index++) {
+        Score += Math.abs(s.charCodeAt(index - 1) - s.charCodeAt(index));
     }
     return Score;
 };

291f42f74239b40f95615ff3a220a697c2694330
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-02-15 22:09:07 +0530

    Time: 1 ms (7.69%) | Memory: 49.2 MB (46.79%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.js
+++ 1944-truncate-sentence/truncate-sentence.js
@@ -3,6 +3,23 @@
  * @param {number} k
  * @return {string}
  */
-var truncateSentence = function(s, k) {
-   return s.split(' ',k).join(' ')
+var truncateSentence = function (s, k) {
+    // return s.split(' ', k).join(' ');
+
+    let newSentence = '';
+    let lengthOfNewSentence = 0
+    for (let i = 0; i < s.length; i++) {
+        if (lengthOfNewSentence == k) {
+            return newSentence
+        }
+        if (s[i] === ' ') {
+            lengthOfNewSentence++;
+            if (lengthOfNewSentence !== k) {
+                newSentence += ' ';
+            }
+        } else if (s[i] !== ' ') {
+            newSentence += s[i];
+        }
+    }
+    return newSentence
 };

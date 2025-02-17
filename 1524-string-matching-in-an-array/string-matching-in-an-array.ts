5d9e0609bce40038c36aa77d7e56a95e07c70c4e
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-02-18 01:18:20 +0530

    Time: 0 ms (100.00%) | Memory: 51.9 MB (54.25%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.ts
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.ts
@@ -1,5 +1,5 @@
 function stringMatching(words: string[]): string[] {
-    const res = [];
+    const res: string[] = [];
     for (let i = 0; i < words.length; i++) {
         for (let j = 0; j < words.length; j++) {
             if (i !== j && words[j].includes(words[i])) {

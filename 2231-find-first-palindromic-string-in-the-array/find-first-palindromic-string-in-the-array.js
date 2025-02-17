0b180dbb5fc56c31d971d2f46ea99baac8075b7e
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-02-17 15:54:43 +0530

    Time: 4 ms (77.07%) | Memory: 53.8 MB (97.93%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.js
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.js
@@ -3,12 +3,23 @@
  * @return {string}
  */
 var firstPalindrome = function (words) {
-    const isPalindrome = word => {
-        for (let i = 0; i < word.length / 2; i++) {
-            if (word[i] !== word[word.length - 1 - i]) return false;
-        };
+    // const isPalindrome = word => {
+    //     for (let i = 0; i < word.length / 2; i++) {
+    //         if (word[i] !== word[word.length - 1 - i]) return false;
+    //     };
+    //     return true;
+    // }
+    // for (let word of words) if (isPalindrome(word)) return word;
+    // return ""
+
+    const isPalindrome = w => {
+        for (let i = 0; i < w.length / 2; i++) {
+            if (w[i] !== w[w.length - 1 - i]) return false;
+        }
         return true;
     }
-    for (let word of words) if (isPalindrome(word)) return word;
-    return ""
+
+    for (const c of words) if (isPalindrome(c)) return c;
+
+    return "";
 };

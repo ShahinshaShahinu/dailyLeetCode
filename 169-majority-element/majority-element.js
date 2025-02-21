8330a84b17b449d50dd938f250be2bce318762bb
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-02-21 21:52:11 +0530

    Time: 0 ms (100.00%) | Memory: 56.5 MB (5.10%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -2,19 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
     let element = null;
-    let count = 0 ;
+    let count = 0;
 
-    for(let i=0;i<nums.length;i++){
-        if(count === 0){
+    for (let i = 0; i < nums.length; i++) {
+        if (count === 0) {
             element = nums[i];
-            count = 1 ;
-        }else if(element === nums[i]){
+            count = 1;
+        } else if (element === nums[i]) {
             count++
-        }else{
+        } else {
             count--
         }
     }
-    return element ;
+    return element;
 };

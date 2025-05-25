a47443fe6cf149e701ba716f7ab93b0fa56efff6
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-05-25 18:56:21 +0530

    Time: 34 ms (28.26%) | Memory: 55.5 MB (34.56%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,12 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
                 return [i, j];  // Return the indices of the two numbers
-         }
-        }
-    }
+         };
+        };
+    };
+
     return [-1, -1];  // Return [-1, -1] if no solution is found
 };

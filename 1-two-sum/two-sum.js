dcf82b854b8654111757810973c67d437a19bc95
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-06-30 13:35:17 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (63.74%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,25 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-
-
+   let prev = {};
     
-    const map = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        map.set(nums[i], i);
-    }
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (map.has(complement) && map.get(complement) !== i) {
-            return [i, map.get(complement)];
+    for(let i=0;i<nums.length;i++){
+        let diff = target - nums[i];
+        if(diff in prev){
+            return [prev[diff],i];
         }
-    };
-    // If no valid pair is found, return an empty array
-    return [];
-
-
-
-
-
-
+        prev[nums[i]] = i ;  // this is a key assign a value ..
+    }
+    return 0;
 };

75165b6629a5282d0a8d9a153fe6bffc9b05da71
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-07-16 17:08:42 +0530

    Time: 35 ms (89.13%) | Memory: 53.7 MB (25.69%) - LeetSync

--- 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -5,30 +5,24 @@
 var expect = function (val) {
     return {
         toBe: function (value) {
-            if (val === value) return true;
-            throw new Error("Not Equal")
+            if (val === value) {
+                return true;
+            } else {
+                throw new Error("Not Equal")
+            }
         },
         notToBe: function (value) {
-            if (val !== value) return true;
-            throw new Error("Equal")
+            if (val !== value) {
+                return true;
+            } else {
+                throw new Error("Equal")
+            }
         }
 
     };
 };
 
 
-try {
-    const result = expect(5).toBe(5);
-    console.log({ "value": result })
-} catch (error) {
-    console.log({ error: error.message })
-}
-try {
-    const result = expect(5).notToBe(5)
-    console.log({ "value": result })
-} catch (error) {
-    console.log({ error: error.message })
-}
 
 /**
  * expect(5).toBe(5); // true

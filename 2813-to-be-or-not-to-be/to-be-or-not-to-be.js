d7cce45195dda8306518a28107814ec1aeeb45bd
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-07-16 17:07:22 +0530

    Time: 40 ms (67.60%) | Memory: 53.8 MB (17.55%) - LeetSync

--- 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -16,6 +16,20 @@ var expect = function (val) {
     };
 };
 
+
+try {
+    const result = expect(5).toBe(5);
+    console.log({ "value": result })
+} catch (error) {
+    console.log({ error: error.message })
+}
+try {
+    const result = expect(5).notToBe(5)
+    console.log({ "value": result })
+} catch (error) {
+    console.log({ error: error.message })
+}
+
 /**
  * expect(5).toBe(5); // true
  * expect(5).notToBe(5); // throws "Equal"

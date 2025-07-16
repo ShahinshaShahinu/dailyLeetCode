bf2cf8d66e84c031eff8b2a2ad95bb322b6ba3e7
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-07-16 17:51:46 +0530

    Time: 41 ms (60.13%) | Memory: 53.9 MB (23.99%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -4,8 +4,11 @@
  * @return {number[]}
  */
 var map = function (arr, fn) {
+
+    let result = [];
     for (let i = 0; i < arr.length; i++) {
-        arr[i] = fn(arr[i], i)
+        result.push(fn(arr[i], i));
     };
-    return arr;
+    return result;
+
 };

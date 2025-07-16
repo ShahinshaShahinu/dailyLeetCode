96b6d7b80e05f374c97cabac9d2aca1d0395bb3b
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-07-16 18:15:15 +0530

    Time: 50 ms (20.64%) | Memory: 54.2 MB (67.96%) - LeetSync

---
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function (nums, fn, init) {
+    let value = 0;
+    let flag = false;
+    for (let i = 0; i < nums.length; i++) {
+        value = fn(!flag ? init : value, nums[i]);
+        flag = true;
+    }
+    return value === 0 && nums.length === 0 ? init : value;
+};

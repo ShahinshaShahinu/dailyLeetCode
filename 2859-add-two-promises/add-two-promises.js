3cb0b9b4b6a1288f52004ea9c08ead7f92ae2e16
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-07-18 18:30:34 +0530

    Time: 46 ms (83.44%) | Memory: 53.6 MB (78.62%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -4,6 +4,7 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
+    
     const [result1 , result2] = await Promise.all([promise1,promise2])
     return result1 + result2;
 };

0a37ba952d975d9c126adc8ea5db67faf94cb707
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-07-19 20:06:31 +0530

    Time: 0 ms (100.00%) | Memory: 58.4 MB (48.12%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.js
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.js
@@ -3,14 +3,19 @@
  * @return {number}
  */
 var repeatedNTimes = function (nums) {
-    let seen = [];
+   const length = nums.length
 
-    for (let num of nums) {
-        if (seen.includes(num)) {
-            return num;
+    for(let i=0; i<length - 3; i++) {
+        const set = new Set()
+        for(let j=i; j<=i+3; j++){
+            if(set.has(nums[j])){
+                return nums[j]
+            } else{
+                set.add(nums[j])
+            }
         }
-        seen.push(num);
+
     }
 
-    return -1;
+    return false
 };

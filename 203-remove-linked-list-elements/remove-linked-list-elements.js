dfe5f6dae5f93a682911dd0cc933e7cdb9e32441
Muhammed Shahinsha .c <muhammedshahinsha2442@gmail.com>
2025-08-05 12:38:19 +0530

    Time: 2 ms (24.15%) | Memory: 60.8 MB (81.74%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+    if (!head) {
+        return head;
+    }
+
+    while (head) {
+        if (head.val === val) {
+            head = head.next;
+        } else {
+            break;
+        }
+    }
+
+    let current = head?.next;
+    let previous = head;
+
+    while (current) {
+        if (current.val === val) {
+            previous.next = current.next;
+            current = current.next;
+        } else {
+            previous = current;
+            current = current.next;
+        }
+    }
+
+    return head;
+};
